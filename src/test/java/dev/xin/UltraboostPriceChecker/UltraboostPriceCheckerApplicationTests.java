8191faadf0ef664e6235fb42cb0c3941963a43f4
Xin Lai <xin.lai-1@ou.edu>
2024-05-23 22:49:40 -0500

    Added inital spring inital files

---
+++ src/test/java/dev/xin/UltraboostPriceChecker/UltraboostPriceCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.xin.UltraboostPriceChecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltraboostPriceCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
